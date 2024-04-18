5fd14e74a2e6b4e7f7df533dab3026e8339efe2e
Zoody <reopard506@gmail.com>
2024-04-18 17:00:50 +0900

    [✨FEAT]      : DB 연결
      - DB 연결 설정
      - profile 초기화 구현 (profile 여부에 따라 profile 생성)

---
+++ src/lib/db.ts
@@ -0,0 +1,12 @@
+/**
+ * NextJS Prisma Client Instance 중복 생성 문제
+ */
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
