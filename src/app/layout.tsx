e650f20d4a298e9398593afe7165d0d9afd3cd65
Zoody <reopard506@gmail.com>
2024-04-18 16:07:27 +0900

    [✨FEAT]      : Theme mode 구현
       - next-theme, shadcn 을 사용한 theme mode 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
 
+import { ThemeProvider } from "@/components/providers/theme-provider.tsx";
+import { cn } from "@/lib/utils";
+
 const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,8 +21,18 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="Clncord-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
