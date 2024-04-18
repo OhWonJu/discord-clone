f4016003921f3a81e5ec35015e6a1b54de9c00ad
오원주 <reopard506@gmail.com>
2024-04-18 16:10:46 +0900

    [Feat] theme mode 구현 (#2)
    * [📦️PACKAGE]   : clerk, zod 추가
    
      - clerk : SSO 인증
      - zod: schema
    
    * [🔨CHORE]     : gitignore 룰 추가
    
    * [✨FEAT]      : 인증을 위한 기본 설정
    
    * [✨FEAT]      : clerk 을 사용한 OAuth 구현
    
    * [📦️PACKAGE]   : next-theme 추가
    
    * [🎨STYLE]     : main page 불필요 import 구문 제거
    
    * [📦️PACKAGE]   : shadcn dropdown-menu 추가
    
    * [✨FEAT]      : Theme mode 구현
    
       - next-theme, shadcn 을 사용한 theme mode 구현

--- src/app/(main)/(routes)/page.tsx
+++ src/app/(main)/(routes)/page.tsx
@@ -1,11 +1,16 @@
+import { ModeToggle } from "@/components/ModeToggle";
+import { UserButton } from "@clerk/nextjs";
+
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div className="">
       <UserButton afterSignOutUrl="/sign-in" />
+      <ModeToggle />
     </div>
   );
 }
