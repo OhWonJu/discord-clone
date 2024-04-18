7db92254953fffe955aad95e14f74fcc044f2587
Zoody <reopard506@gmail.com>
2024-04-18 16:13:14 +0900

    [🐛FIX]       : main page 잘못된 import 구문 수정

--- src/app/(main)/(routes)/page.tsx
+++ src/app/(main)/(routes)/page.tsx
@@ -1,11 +1,6 @@
 import { ModeToggle } from "@/components/ModeToggle";
 import { UserButton } from "@clerk/nextjs";
 
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
-
-
 export default function Home() {
   return (
     <div className="">
