2c96f75d809975e0f1842c3b35c5e44c0e7b31b0
오원주 <reopard506@gmail.com>
2024-04-21 21:35:42 +0900

    [Feat] Create server modal (#8)
    * [📦️PACKAGE]   : zustand 추가
    
    * [✨FEAT]      : Server Create Modal 구현
    
        - zustand 를 사용하여 modal type 상태에 따라 필요한 모달이 open 될 수 있도록 구현

---
+++ src/components/providers/ModalProvider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+import { CreateServerModal } from "../modals";
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  // server-side 에서 렌더링 되지 않도록 막음 -> 하이드레이션 에러를 우회하기 위해.
+  // 모달은 클라이언트 사이드의 어떠한 액션
+  // - state 변경
+  // - user action
+  // 등으로 인해 동작하므로 서버사이드에서 렌더링 되면 원하는 결과대로 동작하지 않음
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <>
+      <CreateServerModal />
+    </>
+  );
+};
+
+export default ModalProvider;
