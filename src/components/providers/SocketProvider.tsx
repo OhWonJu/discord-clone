72a2bf41fd0ad2f8b49a17cfef22185e418d609a
Zoody <reopard506@gmail.com>
2024-04-24 21:54:53 +0900

    [✨FEAT]      : socket 통신 provider 구현

---
+++ src/components/providers/SocketProvider.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import {
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { io as ClientIO } from "socket.io-client";
+
+type SocketContextType = {
+  socket: any | null;
+  isConnected: boolean;
+};
+
+const SocketContext = createContext<SocketContextType>({
+  socket: null,
+  isConnected: false,
+});
+
+export const useSocket = () => {
+  return useContext(SocketContext);
+};
+
+export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
+  const [socket, setSocket] = useState(null);
+  const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    const socketInstance = new (ClientIO as any)(
+      // default (localhost) 로 env 에 지정됨. 단 배포시 배포된 주소로 지정해주어야 함.
+      process.env.NEXT_PUBLIC_SITE_URL!,
+      { path: "/api/socket/io", addTrailingSlash: false }
+    );
+
+    socketInstance.on("connect", () => {
+      setIsConnected(true);
+    });
+
+    socketInstance.on("disconnect", () => {
+      setIsConnected(false);
+    });
+
+    setSocket(socketInstance);
+
+    return () => {
+      socketInstance.disconnect();
+    };
+  }, []);
+
+  return (
+    <SocketContext.Provider value={{ socket, isConnected }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
