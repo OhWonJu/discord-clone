c4cefb12103b5a0eba904480869d941f55b27845
Zoody <reopard506@gmail.com>
2024-04-24 17:26:31 +0900

    [✨FEAT]      : Chat Header 구현
      - channel type Header 구현
      - member type Header 미구현

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as ActionTooltip } from "./ActionTooltip";
 export { default as FileUpload } from "./FileUpload";
+export { default as MobileToggle } from "./MobileToggle";
 export { default as ModeToggle } from "./ModeToggle";
 export { default as UserAvatar } from "./UserAvatar";
