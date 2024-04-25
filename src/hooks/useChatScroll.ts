3f526b388323688ffbfb4b6780cc06a5fdd02bdc
오원주 <reopard506@gmail.com>
2024-04-25 19:23:37 +0900

    [Feat] Chat Scroll hook 구현 (#29)
    - 페이지 로딩 완료 여부에 따라 bottom 으로 스크롤
       - 페이지 상단으로 스크롤 시 fetch more 호출
       - 페이지 하단과 차이가 별로 없는 경우 auto scroll block
       - 새 데이터 도착시 count 값 deps 에 반응하여 auto scroll 동작

---
+++ src/hooks/useChatScroll.ts
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+
+interface ChatScrollProps {
+  chatRef: React.RefObject<HTMLDivElement>;
+  bottomRef: React.RefObject<HTMLDivElement>;
+  shouldLoadMore: boolean;
+  loadMore: () => void;
+  count: number;
+}
+
+const useChatScroll = ({
+  chatRef,
+  bottomRef,
+  shouldLoadMore,
+  loadMore,
+  count,
+}: ChatScrollProps) => {
+  const [hasInit, setHasInit] = useState(false);
+
+  // when scroll on top =>  fetch loadMore data
+  useEffect(() => {
+    const topDiv = chatRef?.current;
+
+    const handleScroll = () => {
+      const scrollTop = topDiv?.scrollTop;
+
+      if (scrollTop === 0 && shouldLoadMore) {
+        loadMore();
+      }
+    };
+
+    topDiv?.addEventListener("scroll", handleScroll);
+
+    return () => topDiv?.removeEventListener("scroll", handleScroll);
+  }, [shouldLoadMore, loadMore, chatRef]);
+
+  // when new Data arrived scroll to bottom
+  useEffect(() => {
+    const bottomDiv = bottomRef?.current;
+    const topDiv = chatRef?.current;
+
+    const shouldAutoScroll = () => {
+      if (!hasInit && bottomDiv) {
+        setHasInit(true);
+        return true;
+      }
+
+      if (!topDiv) return false;
+
+      // 유저가 search 중인 경우 scroll event interrupt
+      const distanceFromBottom =
+        topDiv.scrollHeight - topDiv.scrollTop - topDiv.clientHeight;
+
+      return distanceFromBottom <= 100;
+    };
+
+    if (shouldAutoScroll()) {
+      console.log("SCROLL TO");
+      setTimeout(() => {
+        bottomRef.current?.scrollIntoView({ behavior: "smooth" }), 100;
+      });
+    }
+  }, [bottomRef, chatRef, count, hasInit]);
+};
+
+export default useChatScroll;
