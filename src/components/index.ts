b9baaa330e0b7c965abf103903459237b42cdce0
오원주 <reopard506@gmail.com>
2024-04-25 10:48:40 +0900

    [Feat] socket io setup (#22)
    * [📦️PACKAGE]   : socket 통신 추가
    
       - socket.io
       - socket.io-client
    
    * [📦️PACKAGE]   : shadcn component 추가
    
       - badge
    
    * [✨FEAT]      : socket 통신 provider 구현

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@ export { default as ActionTooltip } from "./ActionTooltip";
 export { default as FileUpload } from "./FileUpload";
 export { default as MobileToggle } from "./MobileToggle";
 export { default as ModeToggle } from "./ModeToggle";
+export { default as SocketIndicator } from "./SocketIndicator";
 export { default as UserAvatar } from "./UserAvatar";
--- src/components/providers/index.ts
+++ src/components/providers/index.ts
@@ -1,2 +1,3 @@
 export { default as ModalProvider } from "./ModalProvider";
+export { SocketProvider } from "./SocketProvider";
 export { default as ThemeProvider } from "./ThemeProvider";
